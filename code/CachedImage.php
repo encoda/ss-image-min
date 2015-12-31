262a0f82e74bb4275f6dbcb009d5cd3aeecdc290
helpfulrobot <chris@silverstripe.com>
2016-01-01 02:43:27 +1300

    Converted to PSR-2

--- code/CachedImage.php
+++ code/CachedImage.php
@@ -1,24 +1,30 @@
 <?php
 
-class CachedImage extends Image {
+class CachedImage extends Image
+{
 
-  public function RelativeLink() {
-    return $this->getCachedImage();
-  }
-
-  public function onBeforeWrite() {
-    parent::onBeforeWrite();
-    $this->getCachedImage();
-  }
-
-  public function getCachedImage() {
-    if ($this->isInDB()) {
-      return SSImageMin::get_cached_image($this->getFullPath());
-    } else {
-      if (!$this->exists()) return;
-      return SSImageMin::get_cached_image(Director::baseFolder() . '/' . $this->fileName);
+    public function RelativeLink()
+    {
+        return $this->getCachedImage();
     }
-  }
+
+    public function onBeforeWrite()
+    {
+        parent::onBeforeWrite();
+        $this->getCachedImage();
+    }
+
+    public function getCachedImage()
+    {
+        if ($this->isInDB()) {
+            return SSImageMin::get_cached_image($this->getFullPath());
+        } else {
+            if (!$this->exists()) {
+                return;
+            }
+            return SSImageMin::get_cached_image(Director::baseFolder() . '/' . $this->fileName);
+        }
+    }
 
   /**
    * Gets the absolute URL accessible through the web.
@@ -26,8 +32,9 @@ class CachedImage extends Image {
    * @uses Director::absoluteBaseURL()
    * @return string
    */
-  public function getAbsoluteURL() {
-    return Director::absoluteBaseURL() . $this->RelativeLink();
+  public function getAbsoluteURL()
+  {
+      return Director::absoluteBaseURL() . $this->RelativeLink();
   }
 
   /**
@@ -36,8 +43,8 @@ class CachedImage extends Image {
    * @uses Director::baseURL()
    * @return string
    */
-  public function getURL() {
-    return Director::baseURL() . $this->RelativeLink();
+  public function getURL()
+  {
+      return Director::baseURL() . $this->RelativeLink();
   }
-
 }
